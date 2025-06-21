0a3be51ac642d08fdbdaab26374f91ce926bcc8b
iaryser <timo.ryser@stud.hslu.ch>
2025-06-21 19:23:21 +0200

    Initializing repository for ads-practice

---
+++ Uebungen/uebung01/al/aufgabe03/recursive_sum.py
@@ -0,0 +1,24 @@
+
+# HSLU / ICS/AIML : Modul ADS : Algorithmen & Datenstrukturen
+# Path   : uebung01/al/aufgabe03
+# Version: Mon Feb 17 19:34:04 CET 2025
+
+def recursive_sum(n):
+  
+  # TODO: Implement here...
+  pass
+  
+  
+if __name__ == '__main__':  
+  
+  n = 100
+  print("Sum of 0..100 recursively                    = ", recursive_sum(n))
+  print("Sum of 0..100 explicitly : n * (n + 1) / 2)  = ", (n * (n + 1) // 2))
+
+
+  """ Session-Log:
+
+  Sum of 0..100 recursively                    = 5050
+  Sum of 0..100 explicitly : n * (n + 1) / 2)  = 5050
+
+  """  
