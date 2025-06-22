f22e0f88ca1d95703c552d03e522427290351667
iaryser <timo.ryser@stud.hslu.ch>
2025-06-22 10:04:41 +0200

    initializing - exercise two

---
+++ Uebungen/uebung02/al/aufgabe02/dequeue_empty_exception.py
@@ -0,0 +1,10 @@
+
+# HSLU / ICS/AIML : Modul ADS : Algorithmen & Datenstrukturen
+# Path   : uebung02/al/aufgabe02
+# Version: Sun Feb 23 20:27:43 CET 2025
+
+class DequeEmptyException(Exception):
+
+    def __init__(self, err):
+      super().__init__(err)
+
---
+++ Uebungen/uebung02/al/aufgabe03/empty_stack_exception.py
@@ -0,0 +1,11 @@
+
+# HSLU / ICS/AIML : Modul ADS : Algorithmen & Datenstrukturen
+# Path   : uebung02/al/aufgabe03
+# Version: Sun Feb 23 20:28:21 CET 2025
+
+class EmptyStackException(Exception):
+
+    def __init__(self, err):
+      super().__init__(err)
+      
+
