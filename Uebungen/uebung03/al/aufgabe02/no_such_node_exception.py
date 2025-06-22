bf133809fe03b51b87e727ab6dd3d2565a4c2248
iaryser <timo.ryser@stud.hslu.ch>
2025-06-22 13:52:45 +0200

    initializing - exercise three

---
+++ Uebungen/uebung03/al/aufgabe02/no_such_node_exception.py
@@ -0,0 +1,11 @@
+
+# HSLU / ICS/AIML : Modul ADS : Algorithmen & Datenstrukturen
+# Path   : uebung03/al/aufgabe02
+# Version: Sun Mar  2 12:51:33 CET 2025
+
+class NoSuchNodeException(Exception):
+
+    def __init__(self, err):
+      super().__init__(err)
+      
+
