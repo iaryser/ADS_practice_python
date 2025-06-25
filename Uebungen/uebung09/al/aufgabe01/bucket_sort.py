d5b9fc649ffb4e5e0b1efaf3b05789ab1cbb6255
iaryser <timo.ryser@stud.hslu.ch>
2025-06-25 14:51:56 +0200

    solved - execise nine

--- Uebungen/uebung09/al/aufgabe01/bucket_sort.py
+++ Uebungen/uebung09/al/aufgabe01/bucket_sort.py
@@ -16,11 +16,21 @@ def bucket_sort(sequence, n):
   buckets = [None] * n
   
   # TODO: Implement here Phase 1 ...
+  while len(sequence) != 0:
+    key = sequence.popleft()
+    if buckets[key] == None:
+      buckets[key] = [key]
+    else:
+      buckets[key].append(key)
  
   pretty_print(buckets)
   
   # TODO: Implement here Phase 2 ...
-    
+  for i in range(n):
+    while not buckets[i] == None and len(buckets[i]) != 0:
+        e = buckets[i][0]
+        buckets[i].remove(e)
+        sequence.append(e) 
 
 def pretty_print(buckets):
   print(str(buckets).replace("deque", "").replace("(", "").replace(")", ""))
